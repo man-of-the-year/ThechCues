1ed8516481639772febae5c3b19968b7a8804638
Wander <80575375+man-of-the-year@users.noreply.github.com>
2022-06-08 13:20:44 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+const main = document.querySelector('.main');
+const buttonBURGER = document.querySelector('.burger__button');
+const wrapper = document.querySelector('.burger__wrapper');
+const nav = document.querySelector('.burger__nav');
+const title = document.querySelector('.burger__title');
+const buttons = document.querySelectorAll('button');
+const topLine = document.querySelector('.burger__topLine');
+const midLine = document.querySelector('.burger__midLine');
+const botLine = document.querySelector('.burger__botLine');
+
+
+for (let i = 0; i < buttons.length; i++) {
+  const button = buttons[i];
+  button.onclick = function() {
+    window.alert('This feature is not configured yet')
+  }
+}
+
+buttonBURGER.onclick = function() {
+  wrapper.classList.toggle('_active');
+  nav.classList.toggle('_active');
+  title.classList.toggle('_active');
+  main.classList.toggle('_inactive');
+  topLine.classList.toggle('_active');
+  midLine.classList.toggle('_active');
+  botLine.classList.toggle('_active');
+}
